2ea44bb23e903e6aab58adc4ab06b13608cd5545
Todd Fraser <toddfraser@Todds-iMac.home>
2019-05-05 01:01:16 +0200

    meta fix, removed unsued component declarations

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from "./contact.module.scss"
 import { Link } from "react-router-dom";
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Sunny from './Sunny';
 import Logo from './Logo';
 import MainBody from './MainBody';
