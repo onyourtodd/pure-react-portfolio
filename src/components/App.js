7c68a92d1e5fd3e0e202249cfb6da26840a356f6
Todd Fraser <toddfraser@Todds-iMac.home>
2019-05-07 20:21:52 +0200

    working state - pre GSAP installation

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import About from "./About";
 import Contact from "./Contact";
 import {Route, Switch } from "react-router-dom";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
+import './app.css';
 
 class App extends Component {
     render() {
@@ -11,7 +12,7 @@ class App extends Component {
             <>
                 <Route render={({location}) => (
                     <TransitionGroup>
-                        <CSSTransition key={location.key} classNames="fade" >
+                        <CSSTransition key={location.key} classNames="slide" >
                             <Switch location={location}>
                                 <Route path="/about" component={About}/>
                                 <Route path="/contact" component={Contact}/>
